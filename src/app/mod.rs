dcf114dfce556e8f992f98734b3685f9ef5d844a
Jan Dahlke <jan.dahlke@akquinet.de>
2023-07-18 10:58:29 +0200

    controlled form and remove slurs

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,4 +1,4 @@
-use leptos::*;
+use leptos::{ev::SubmitEvent, *};
 
 #[component]
 pub fn Counter(cx: Scope) -> impl IntoView {
@@ -22,5 +22,50 @@ pub fn Counter(cx: Scope) -> impl IntoView {
 
         <p>{move || count.get()}</p>
         </div>
+    }
+}
+
+#[component]
+pub fn uncontrolled_form(cx: Scope) -> impl IntoView {
+    // import the type for <input>
+    use leptos::html::Input;
+
+    let (name, set_name) = create_signal(cx, "Uncontrolled".to_string());
+    let input_element: NodeRef<Input> = create_node_ref(cx);
+    let on_submit = move |ev: SubmitEvent| {
+        ev.prevent_default();
+
+        let value = input_element.get().expect("<input> to exist").value();
+        set_name.set(value);
+    };
+
+    view! {
+        cx,
+        <form on:submit=on_submit>
+            <input
+                type="text"
+                ref=input_element
+                prop:value=name.get()
+            />
+            <p>"name is:  " {name}</p>
+        </form>
+    }
+}
+
+#[component]
+pub fn controlled_form(cx: Scope) -> impl IntoView {
+    let (name, set_name) = create_signal(cx, "Controlled".to_string());
+    view! { cx,
+      <div>
+        <input
+          type="text"
+          on:input=move |ev| {
+            set_name.set(event_target_value(&ev));
+          }
+          prop:value=name.get()
+
+        />
+        <p>"name is:  " {name}</p>
+      </div>
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use leptos::{ev::SubmitEvent, *};
+use leptos::*;
 mod App;
 
 fn main() {
@@ -6,35 +6,9 @@ fn main() {
         view! { cx,
         <div>
          <App::Counter />
-         <UncontrolledForm />
+         <App::UncontrolledForm />
+         <App::ControlledForm />
         </div>
         }
     })
-}
-
-#[component]
-fn uncontrolled_form(cx: Scope) -> impl IntoView {
-    // import the type for <input>
-    use leptos::html::Input;
-
-    let (name, set_name) = create_signal(cx, "Uncontrolled".to_string());
-    let input_element: NodeRef<Input> = create_node_ref(cx);
-    let on_submit = move |ev: SubmitEvent| {
-        ev.prevent_default();
-
-        let value = input_element.get().expect("<input> to exist").value();
-        set_name.set(value);
-    };
-
-    view! {
-        cx,
-        <form on:submit=on_submit>
-            <input
-                type="text"
-                ref=input_element
-                prop:value=name.get()
-            />
-            <p>"name is:  " {name}</p>
-        </form>
-    }
 }
