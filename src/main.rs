f48e2e5930eb887172f30e630175e3fc4b11f8f6
Jan Dahlke <jan.dahlke@akquinet.de>
2023-07-18 19:48:23 +0200

    routing test

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use leptos::*;
+use leptos_router::*;
 
 #[allow(non_snake_case)]
 mod App;
@@ -7,16 +8,47 @@ fn main() {
     mount_to_body(|cx| {
         view! { cx,
         <div>
-            <header style="height:10vh;width:100%;display:flex;align-items:center;justify-content:center;background:black;">
-                <a href="/counter" style="margin:0 10px;color:white;">Counter</a>
-                <a href="/uncontrolled_form" style="margin:0 10px;color:white;">Uncontrolled Form</a>
-                <a href="/controlled_form" style="margin:0 10px;color:white;">Controlled Form</a>
-            </header>
-
-            <App::Counter />
-            <App::UncontrolledForm />
-            <App::ControlledForm />
-            </div>
-            }
+            <Routable />
+        </div>
+        }
     })
+}
+
+#[component]
+fn Routable(cx: Scope) -> impl IntoView {
+    view! { cx,
+     <Router>
+
+        <header style="height:10vh;width:100%;display:flex;align-items:center;justify-content:center;background:black;">
+        <a href="/" style="margin:0 10px;color:white;">Home</a>
+        <a href="/counter" style="margin:0 10px;color:white;">Counter</a>
+        <a href="/uncontrolled" style="margin:0 10px;color:white;">Uncontrolled Form</a>
+        <a href="/controlled" style="margin:0 10px;color:white;">Controlled Form</a>
+    </header>
+
+        <main>
+        <Routes>
+            <Route
+                path="/counter"
+                view=move |cx| view! {cx, <App::Counter /> }
+                ></Route>
+            <Route
+                path="/uncontrolled"
+                view=move |cx| view! {cx, <App::UncontrolledForm /> }
+                ></Route>
+            <Route
+                path="/controlled"
+                view=move |cx| view! {cx, <App::ControlledForm /> }
+                ></Route>
+            <Route
+                path="/"
+                view=move |cx| view! {cx, <div>"hello leptos"</div> }
+                ></Route>
+
+        </Routes>
+        </main>
+
+
+    </Router>
+    }
 }
