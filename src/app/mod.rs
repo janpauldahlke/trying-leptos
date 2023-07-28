55c64f4c7e439da4bcd0a93dcbf21cdb539a7ea3
HagbardCeline <janpauldahlke@gmail.com>
2023-07-28 08:04:40 +0200

    how the frkng f*** can we ship the styles! macro into this?

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -2,11 +2,14 @@ use leptos::{ev::SubmitEvent, *};
 
 #[component]
 pub fn Counter(cx: Scope) -> impl IntoView {
-    let (count, set_count) = create_signal(cx, 0);
 
+    let sheet = style_sheet!("./../styles/main.css");
+
+    let (count, set_count) = create_signal(cx, 0);
     view! { cx,
       <div>
         <button
+            class="button"
             on:click=move |_| {
                 set_count.update(|n| { *n += 1
                 });
